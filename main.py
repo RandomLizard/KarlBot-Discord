68a027274b2fe73910ae9b38c48db1e009f0289c
Tate Wilson <tatekwilson@proton.me>
2024-10-03 21:53:47 -0700

    Added music framework - Got join and leave working

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import discord
-from discord import app_commands
 import os
-from dotenv import load_dotenv
-from discord.ext import tasks, commands
+import asyncio
 
+from dotenv import load_dotenv
+from discord import app_commands
+from discord.ext import tasks, commands
 from typing import Literal, Union, NamedTuple
 from enum import Enum
+from yt_dlp import YoutubeDL
 
 #Install Link: https://discord.com/oauth2/authorize?client_id=1065456624665366670&permissions=2150657600&integration_type=0&scope=bot+applications.commands
 
 load_dotenv()
-MY_GUILD=discord.Object(id=os.getenv("DIMMADOME")) #replace this with your desired guild ID
+MY_GUILD=discord.Object(id=os.getenv("DUMPTRUCK")) #replace this with your desired guild ID
 TOKEN = os.getenv("DEV_DISCORD_TOKEN")
+
+
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' 
+}
 
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
@@ -22,6 +40,23 @@ class MyClient(discord.Client):
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get("title")
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+ytdl = YoutubeDL(ytdl_format_options)
 
 intents = discord.Intents.default()
 client = MyClient(intents=intents)
@@ -69,11 +104,26 @@ async def secret(interaction: discord.Interaction):
     await interaction.response.send_message(f"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic.demilked.com%2Fwp-content%2Fuploads%2F2018%2F03%2F5aaa1cc4c422b-funny-weird-wtf-stock-photos-28-5a3a5b135f099__700.jpg")
     
 
-@client.tree.command(name="Play")
-@app_commands.describe(source="Either a youtube link or a search for something to play")
-async def play_music(interaction: discord.Interaction):
-    """Plays audio from a youtube video"""
+# Music related commands below
 
+@client.tree.command(name="join")
+async def join(interaction: discord.Interaction):
+    """Joins the user's current voice channel"""
 
+    if not interaction.user.voice:
+        await interaction.response.send_message("I can't join you because you're not in a voice channel.")
+    else:
+        channel = interaction.user.voice.channel
+        await channel.connect()
+        await interaction.response.send_message("Ready to rock")
+
+@client.tree.command(name="leave")
+async def leave(interaction: discord.Interaction):
+    voice_client = interaction.guild.voice_client
+    if voice_client.is_connected():
+        await interaction.response.send_message("cya!")
+        await voice_client.disconnect()
+    else:
+        await interaction.response.send_message("I'm not connected to a voice channel.")
 
 client.run(TOKEN)
